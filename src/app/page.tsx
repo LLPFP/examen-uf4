b180454237f1bba4752cf31a91badc8c99882967
Luis López <lopezpuigluis@fpllefia.com>
2025-05-28 19:59:51 +0200

    footer actualizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { HiArrowsUpDown } from "react-icons/hi2";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center pt-20  text-center">
+      <main className="flex min-h-screen flex-col items-center pt-20 mb-20  text-center">
         <section className="flex flex-col items-center justify-center px-4 md:px-8">
           <h1 className="text-3xl font-bold md:text-4xl lg:text-6xl max-w-4xl text-center">
             Information you need during on-call emergencies
@@ -22,58 +22,58 @@ export default function Home() {
           <Image
             src="/images/imgSection1.png"
             alt="Imagen Content Overview"
-            className="w-full max-w-7xl mt-10 shadow-lg"
+            className="w-full max-w-8xl mt-10 shadow-lg"
             width={1200}
             height={600}
           />
         </section>
 
         <section className="flex flex-col items-center justify-center mt-20 pt-10">
-          <h2 className="text-3xl font-bold md:text-5xl lg:text-5xl max-w-4xl">
+          <h2 className="text-5xl font-bold md:text-6xl lg:text-7xl max-w-5xl text-center">
             Quick Solutions, less stress
           </h2>
 
-          <div className="flex flex-row items-start justify-center mt-15 space-x-2">
+          <div className="flex flex-col md:flex-row items-center md:items-start justify-center mt-15 space-y-6 md:space-y-0 md:space-x-6">
             <div className="flex flex-col text-left max-w-md p-8 rounded-lg shadow-lg">
-              <CiStopwatch className="w-16 h-16 text-white mb-6 border-2  border-gray-800 rounded p-3 max-w-3xl" />
+              <CiStopwatch className="w-16 h-16 text-white mb-6 border-2 border-gray-800 rounded p-3 mx-auto md:mx-0" />
 
-              <h3 className="text-3xl font-bold mt-5 mb-5">
+              <h3 className="text-3xl font-bold mt-5 mb-5 text-center md:text-left">
                 Fix emergencies fast
               </h3>
-              <p className="text-lg mt-3 mb-5 ">
+              <p className="text-lg mt-3 mb-5 text-center md:text-left">
                 Save 20-30 minutes per on-call ticket - no more searching for
                 relevant issues and runbooks
               </p>
             </div>
             <div className="flex flex-col text-left max-w-md p-8 rounded-lg shadow-lg">
-              <HiArrowsUpDown className="w-16 h-16 text-white mb-6 border-2 border-gray-800 rounded p-3 max-w-3xl" />
+              <HiArrowsUpDown className="w-16 h-16 text-white mb-6 border-2 border-gray-800 rounded p-3 mx-auto md:mx-0" />
 
-              <h3 className="text-3xl font-bold mt-5 mb-5">
+              <h3 className="text-3xl font-bold mt-5 mb-5 text-center md:text-left">
                 Universally compatible
               </h3>
-              <p className="text-lg mt-3 mb-5">
+              <p className="text-lg mt-3 mb-5 text-center md:text-left">
                 Works with PagerDuty, Jira, or custom Slack alerts—Pandem
                 integrates with any system
               </p>
             </div>
             <div className="flex flex-col text-left max-w-md p-8 rounded-lg shadow-lg">
-              <CiStopwatch className="w-16 h-16 text-white mb-6 border-2  border-gray-800 rounded p-3 max-w-3xl" />
+              <CiStopwatch className="w-16 h-16 text-white mb-6 border-2 border-gray-800 rounded p-3 mx-auto md:mx-0" />
 
-              <h3 className="text-3xl font-bold mt-5 mb-5">
+              <h3 className="text-3xl font-bold mt-5 mb-5 text-center md:text-left">
                 Secure for your org
               </h3>
-              <p className="text-lg mt-3 mb-5">
+              <p className="text-lg mt-3 mb-5 text-center md:text-left">
                 We keep your data safe by taking top security measures.
               </p>
             </div>
           </div>
         </section>
 
-        <section className="flex flex-col items-center justify-center pt-20 mt-20">
-          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl max-w-4xl">
+        <section className="flex flex-col items-center justify-center pt-20 mt-20 px-4 md:px-8">
+          <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl max-w-4xl text-center">
             Instant setup, no custom code
-          </h1>
-          <p className="text-xl mt-4 max-w-2xl mt-10">
+          </h2>
+          <p className="text-lg mt-4 max-w-2xl md:text-xl mt-10 text-center">
             Invite the bot, pick a channel, and you&apos;re set—no custom code
             needed, and no vendor lock-in.
           </p>
@@ -81,14 +81,14 @@ export default function Home() {
           <Image
             src="/images/imgSection1.png"
             alt="Imagen Content Overview"
-            className="w-full max-w-7xl mt-12 shadow-lg"
+            className="w-full max-w-3xl mt-12 shadow-lg"
             width={1200}
             height={600}
           ></Image>
         </section>
 
-        <section className="flex flex-col items-center justify-center pt-20 mt-20">
-          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl max-w-4xl">
+        <section className="flex flex-col items-center justify-center pt-20 mt-20 mb-15">
+          <h1 className="text-5xl font-bold md:text-5xl lg:text-6xl max-w-4xl">
             Get in touch
           </h1>
           <p className="text-xl mt-4 max-w-2xl mt-10">
--- src/misComponentes/Footer.tsx
+++ src/misComponentes/Footer.tsx
@@ -1,44 +1,29 @@
 import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
 import Link from "next/link";
-
-import { PiFingerprint } from "react-icons/pi";
+import { HiFingerPrint } from "react-icons/hi2";
 
 export function Footer() {
   return (
-    <Menubar className=" bg-black p-4 text-gray-200 shadow-md rounded-none border-b border-black flex flex-row items-center justify-between ">
-      <MenubarMenu>
-        <MenubarTrigger>
-          <PiFingerprint />
+    <Menubar className="bg-black p-4 text-gray-300 shadow-md rounded-none border-b border-black flex flex-row items-center justify-between">
+      <div className="flex justify-center items-center space-x-4 mx-auto">
+        <MenubarMenu>
+          <MenubarTrigger className="text-lg font-bold flex items-center">
+            <HiFingerPrint className="inline-block mr-3 w-6 h-6" />
+            <Link href="/" className="hover:underline">
+              Pandem
+            </Link>
+          </MenubarTrigger>
+        </MenubarMenu>
+      </div>
 
-          <Link href="/">Pandem</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>
-          <Link href="/docs">Docs</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>
-          <Link href="/pricing">Pricing</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>Status</MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>FAQs</MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>Contact Us</MenubarTrigger>
-      </MenubarMenu>
-
-      <MenubarMenu>
-        <MenubarTrigger>Login</MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>Sign Up</MenubarTrigger>
-      </MenubarMenu>
+      <div className="flex justify-center items-center space-x-4 mx-auto">
+        <MenubarMenu>
+          <MenubarTrigger>contact us</MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>terms of service</MenubarTrigger>
+        </MenubarMenu>
+      </div>
     </Menubar>
   );
 }
